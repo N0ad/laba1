ac327a1cca1965ebe1f9cf8121851314fc69b367
N0ad <116006062+N0ad@users.noreply.github.com>
2022-10-18 17:43:53 +0700

    Update BigInt.h

--- BigInt.h
+++ BigInt.h
@@ -61,7 +61,43 @@ public:
 	BigInt& operator--();
 	const BigInt operator--(int) const;
 
-	BigInt& operator+=(const BigInt&);
+	BigInt& operator+=(const BigInt& b) {
+		std::string c = "";
+		std::string st = b.data;
+		int nxt = 0;
+		int d, sum;
+		BigInt res;
+		if (b.min == min) {
+			if (data.length() > st.length()) {
+				for (int i = 0; i <= data.length() - st.length(); i++) {
+					st = "0" + st;
+				}
+				data = "0" + data;
+			}
+			for (int i = 0; i < data.length(); i++) {
+				sum = data[data.length() - i - 1] - '0';
+				d = st[data.length() - i - 1] - '0';
+				sum = sum + d + nxt;
+				nxt = sum / 10;
+				sum = sum % 10;
+				c = lib[sum] + c;
+			}
+			while (1) {
+				if ((c[0] == '0') && (c.length() != 1) {
+					c.erase(0, 1);
+				}
+				else {
+					break;
+				}
+			}
+			res.data = c;
+			res.min = min;
+		}
+		if (res.data == 0) {
+			res.min = 0;
+		}
+		return res;
+	};
 	BigInt& operator*=(const BigInt&);
 	BigInt& operator-=(const BigInt&);
 	BigInt& operator/=(const BigInt&);
@@ -73,12 +109,103 @@ public:
 	BigInt operator+() const;  // unary +
 	BigInt operator-() const;  // unary -
 
-	bool operator==(const BigInt&) const;
-	bool operator!=(const BigInt&) const;
-	bool operator<(const BigInt&) const;
-	bool operator>(const BigInt&) const;
-	bool operator<=(const BigInt&) const;
-	bool operator>=(const BigInt&) const;
+	bool operator==(const BigInt& b) const {
+		if ((b.data == data) && (b.min == min)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	};
+	bool operator!=(const BigInt& b) const {
+		if ((data != b.data) || (min != b.min)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	};
+	bool operator<(const BigInt& b) const {
+		if (min == b.min) {
+			if (min == 1) {
+				if (data.length() == b.data.length()) {
+					if (data > b.data) {
+						return true;
+					}
+					else {
+						return false;
+					}
+				}
+				else {
+					if (data.length() > b.data.length()) {
+						return true;
+					}
+					else {
+						return false;
+					}
+				}
+			}
+			else {
+				if (data.length() == b.data.length()) {
+					if (data < b.data) {
+						return true;
+					}
+					else {
+						return false;
+					}
+				}
+				else {
+					if (data.length()< b.data.length()) {
+						return true;
+					}
+					else {
+						return false;
+					}
+				}
+			}
+		}
+		else {
+			if (min == 1) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+	};
+	bool operator>(const BigInt& b) const {
+		BigInt a;
+		a.min = min;
+		a.data = data;
+		if ((a == b) || (a < b)) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	};
+	bool operator<=(const BigInt& b) const {
+		BigInt a;
+		a.min = min;
+		a.data = data;
+		if (a > b) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	};
+	bool operator>=(const BigInt& b) const {
+		BigInt a;
+		a.min = min;
+		a.data = data;
+		if (a < b) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	};
 
 	operator int() const;
 	operator std::string() const;
