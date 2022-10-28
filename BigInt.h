9e7e042b53d95d8ceba777bef40ef895d0a3ed72
N0ad <116006062+N0ad@users.noreply.github.com>
2022-10-28 10:28:39 +0700

    Update BigInt.h

--- BigInt.h
+++ BigInt.h
@@ -6,6 +6,127 @@
 class BigInt {
 private:
 	char lib[11] = "0123456789";
+	std::string bigintToBin(BigInt b) {
+		BigInt a = 1;
+		BigInt prev;
+		std::string  s = ""
+		while (1) {
+			if (a >= b) {
+				break;
+			}
+			prev = a;
+			a = a + a;
+		}
+		if (a > b) {
+			a = prev;
+		}
+		while (1) {
+			if (a <= b) {
+				s = s + "1";
+				b = b - a;
+			}
+			else {
+				s = s + "0";
+			}
+			if (a.data == "1") {
+				break;
+			}
+			a = a / 2;
+		}
+		return s;
+	};
+	BigInt plus(const BigInt a, BigInt b) {
+		std::string c = "";
+		std::string st = b.data;
+		std::string s = a.data;
+		int nxt = 0;
+		int d, sum;
+		BigInt res;
+		if (s.length() > st.length()) {
+			for (int i = 0; i <= s.length() - st.length(); i++) {
+				st = "0" + st;
+			}
+			s = "0" + s;
+		}
+		for (int i = 0; i < s.length(); i++) {
+			sum = s[s.length() - i - 1] - '0';
+			d = st[s.length() - i - 1] - '0';
+			sum = sum + d + nxt;
+			nxt = sum / 10;
+			sum = sum % 10;
+			c = lib[sum] + c;
+		}
+		while (1) {
+			if ((c[0] == '0') && (c.length() != 1)) {
+				c.erase(0, 1);
+			}
+			else {
+				break;
+			}
+		}
+		res.data = c;
+		res.min = 0;
+		return res;
+	};
+	BigInt minus(const BigInt a, BigInt b) {
+		std::string c = "";
+		std::string st = b.data;
+		std::string s = a.data;
+		int nxt = 0;
+		int d, sum;
+		BigInt res;
+		if (a.data == b.data) {
+			BigInt rtrn;
+			rtrn.min = 0;
+			rtrn.data = "0";
+			return rtrn;
+		};
+		if (s.length() > st.length()) {
+			for (int i = 0; i < s.length() - st.length(); i++) {
+				st = "0" + st;
+			}
+		}
+		if (a > b) {
+			int f = 1;
+		}
+		else {
+			int f = 0;
+		}
+		for (int i = 0; i < s.length(); i++) {
+			sum = s[s.length() - i - 1] - '0';
+			d = st[s.length() - i - 1] - '0';
+			if (f == 1) {
+				sum = sum - d - nxt;
+			}
+			else {
+				sum = d - sum - nxt;
+			}
+			if (sum < 0) {
+				if (sum < -9) {
+					nxt = 2;
+				}
+				else {
+					nxt = 1;
+				}
+			}
+			else {
+				nxt 0;
+			}
+			sum = (10 + (sum % 10)) % 10;
+			c = lib[sum] + c;
+		}
+		while (1) {
+			if ((c[0] == '0') && (c.length() != 1)) {
+				c.erase(0, 1);
+			}
+			else {
+				break;
+			}
+		}
+		res.data = c;
+		res.min = 1 - f;
+		return res;
+	};
 public:
 	BigInt() = default;
 	BigInt(int inp) {
@@ -27,6 +148,14 @@ public:
 	};
 	BigInt(std::string inp) {
 		int a;
+		data = "";
+		if (inp[0] == '-') {
+			min = 1;
+			inp.erase(0, 1);
+		}
+		else {
+			min = 0;
+		}
 		for (int i = 0; i < inp.length(); i++) {
 			a = 0;
 			for (int j = 0; j < 10; j++) {
@@ -37,6 +166,9 @@ public:
 			}
 			if (a == 0) {
 				throw std::invalid_argument("Input data isn't integer!");
+			}
+			else {
+				data = data + inp[i];
 			}
 		}
 	}; // бросать исключение std::invalid_argument при ошибке
@@ -91,31 +223,17 @@ public:
 		int nxt = 0;
 		int d, sum;
 		BigInt res;
+		BigInt a;
+		a.bindata = bindata;
+		a.data = data;
+		a.min = 0;
 		if (b.min == min) {
-			if (data.length() > st.length()) {
-				for (int i = 0; i <= data.length() - st.length(); i++) {
-					st = "0" + st;
-				}
-				data = "0" + data;
-			}
-			for (int i = 0; i < data.length(); i++) {
-				sum = data[data.length() - i - 1] - '0';
-				d = st[data.length() - i - 1] - '0';
-				sum = sum + d + nxt;
-				nxt = sum / 10;
-				sum = sum % 10;
-				c = lib[sum] + c;
-			}
-			while (1) {
-				if ((c[0] == '0') && (c.length() != 1) {
-					c.erase(0, 1);
-				}
-				else {
-					break;
-				}
-			}
-			res.data = c;
+			res = plus(a, b);
 			res.min = min;
+		}
+		else {
+			res = minus(a, b);
+			//вписать изменение знака при переходе через 0
 		}
 		if (res.data == 0) {
 			res.min = 0;
@@ -131,7 +249,7 @@ public:
 		bb.data = b.data;
 		bb.min = 0;
 		while (1) {
-			res = res + a;
+			res = plus(res, a);
 			bb--;
 			if (bb == 0) {
 				break;
@@ -146,57 +264,14 @@ public:
 		int nxt = 0;
 		int d, sum;
 		BigInt res;
+		BigInt a, bb;
+		a.data = data;
+		bb.data = b.data
+		a.min = 0;
+		bb.data = 0;
 		if (b.min == min) {
-			if (data == b.data) {
-				BigInt rtrn;
-				rtrn.min = 0;
-				rtrn.data = "0";
-				return rtrn;
-			};
-			if (data.length() > st.length()) {
-				for (int i = 0; i < data.length() - st.length(); i++) {
-					st = "0" + st;
-				}
-			}
-			if (data > 0) {
-				int f = 1;
-			}
-			else {
-				int f = 0;
-			}
-			for (int i = 0; i < data.length(); i++) {
-				sum = data[data.length() - i - 1] - '0';
-				d = st[data.length() - i - 1] - '0';
-				if (f == 1) {
-					sum = sum - d - nxt;
-				}
-				else {
-					sum = d - sum - nxt;
-				}
-				if (sum < 0) {
-					if (sum < -9) {
-						nxt = 2;
-					}
-					else {
-						nxt = 1;
-					}
-				}
-				else {
-					nxt 0;
-				}
-				sum = (10 + (sum % 10)) % 10;
-				c = lib[sum] + c;
-			}
-			while (1) {
-				if ((c[0] == '0') && (c.length() != 1) {
-					c.erase(0, 1);
-				}
-				else {
-					break;
-				}
-			}
-			res.data = c;
-			res.min = min;
+			res = minus(a, bb);
+			res.min =
 		}
 		else {
 			BigInt y;
